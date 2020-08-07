9d750d6439a11d88e95c6346bec0b631104f08bb
Xuanyu Wu <xuw057@ucsd.edu>
2020-08-07 14:42:21 +0800

    integrated gradients modifs

--- tutorials/int_grad_tutorial.py
+++ tutorials/int_grad_tutorial.py
@@ -1,5 +1,5 @@
 from assets.resnet import ResNet50
-from assets.bilstm import bilstm_net
+from assets.bilstm import bilstm_net_emb
 import paddle.fluid as fluid
 import paddle
 import numpy as np
@@ -36,31 +36,51 @@ def int_grad_example():
 
 
 def nlp_example():
+    #Dataset: https://baidu-nlp.bj.bcebos.com/sentiment_classification-dataset-1.0.0.tar.gz
+    #Pretrained Model: https://baidu-nlp.bj.bcebos.com/sentiment_classification-1.0.0.tar.gz
+    import io
+
+    def load_vocab(file_path):
+        """
+        load the given vocabulary
+        """
+        vocab = {}
+        with io.open(file_path, 'r', encoding='utf8') as f:
+            wid = 0
+            for line in f:
+                if line.strip() not in vocab:
+                    vocab[line.strip()] = wid
+                    wid += 1
+        vocab["<unk>"] = len(vocab)
+        return vocab
+
     def paddle_model(data, alpha, baseline):
         dict_dim = 1256606
         emb_dim = 128
         # embedding layer
         emb = fluid.embedding(input=data, size=[dict_dim, emb_dim])
         emb *= alpha
-        probs = bilstm_net(emb, None, None, dict_dim, is_prediction=True)
+        probs = bilstm_net_emb(emb, None, None, dict_dim, is_prediction=True)
         return emb, probs
 
-    #Dataset: https://baidu-nlp.bj.bcebos.com/sentiment_classification-dataset-1.0.0.tar.gz
-    #Pretrained Model: https://baidu-nlp.bj.bcebos.com/sentiment_classification-1.0.0.tar.gz
     ig = IntGradInterpreter(
         paddle_model,
-        "assets/senta_model/bilstm_model/params",
+        "../../senta_model/bilstm_model/params",
         True,
         model_input_shape=None)
 
-    data = np.array([[
-        1251507, 595755, 1106205, 860907, 1134818, 1106205, 810335, 1134818,
-        4779, 4779, 672177, 280917
-    ]])
+    word_dict = load_vocab("../../senta_model/bilstm_model/word_dict.txt")
+    unk_id = word_dict["<unk>"]
+    reviews = [[
+        '交通', '方便', '；', '环境', '很好', '；', '服务态度', '很好', '', '', '房间', '较小'
+    ]]
 
-    lod = np.array(sum(data.tolist(), []), dtype=np.int64)
-
-    data = fluid.create_lod_tensor(lod, [[12]], fluid.CPUPlace())
+    lod = []
+    for c in reviews:
+        lod.append([word_dict.get(words, unk_id) for words in c])
+    base_shape = [[len(c) for c in lod]]
+    lod = np.array(sum(lod, []), dtype=np.int64)
+    data = fluid.create_lod_tensor(lod, base_shape, fluid.CPUPlace())
 
     avg_gradients = ig.interpret(
         data,
@@ -70,7 +90,15 @@ def nlp_example():
         num_random_trials=1,
         visual=True,
         save_path='ig_test.jpg')
-    print(np.sum(avg_gradients, axis=1))
+
+    sum_gradients = np.sum(avg_gradients, axis=1).tolist()
+    lod = data.lod()
+
+    new_array = []
+    for i in range(len(lod[0]) - 1):
+        new_array.append(sum_gradients[lod[0][i]:lod[0][i + 1]])
+
+    print(new_array)
 
 
 def nlp_example2():
