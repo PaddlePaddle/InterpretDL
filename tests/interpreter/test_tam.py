363873b154b3d3e6fc89afbc26c5d60049db3d98
XianrenYty <842195980@qq.com>
2021-10-13 16:07:41 +0800

    remove print

--- tests/interpreter/test_tam.py
+++ tests/interpreter/test_tam.py
@@ -26,7 +26,7 @@ class TestTAM(unittest.TestCase):
         exp = algo.interpret(img_path, visual=False)
         result = np.array([exp.mean(), exp.std(), exp.min(), exp.max()])
         desired = np.array([0.01317265, 0.01318072, 0.00068325, 0.06299927])
-        print("test_cv", result)
+
         assert_arrays_almost_equal(self, result, desired)
 
 
@@ -38,7 +38,7 @@ class TestTAM(unittest.TestCase):
         exp = algo.interpret(img_path, start_layer=1, visual=False)
         result = np.array([exp.mean(), exp.std(), exp.min(), exp.max()])
         desired = np.array([0.10222234, 0.0989774, 0.00654205, 0.45563712])
-        print("test_cv_layer", result)
+
         assert_arrays_almost_equal(self, result, desired)
 
     def test_cv_class(self):
@@ -49,7 +49,7 @@ class TestTAM(unittest.TestCase):
         exp = algo.interpret(img_path, label=243, visual=False)
         result = np.array([exp.mean(), exp.std(), exp.min(), exp.max()])
         desired = np.array([8.54372094e-03, 1.24013127e-02, 9.54663476e-05, 6.53232618e-02])
-        print("test_cv_class", result)
+
         assert_arrays_almost_equal(self, result, desired)
 
     def test_cv_steps(self):
@@ -60,7 +60,7 @@ class TestTAM(unittest.TestCase):
         exp = algo.interpret(img_path, steps=10, visual=False)
         result = np.array([exp.mean(), exp.std(), exp.min(), exp.max()])
         desired = np.array([0.01300348, 0.01331104, 0.00072738, 0.06281879])
-        print("test_cv_steps", result)
+
         assert_arrays_almost_equal(self, result, desired)
 
     def test_cv_multiple_inputs(self):
@@ -71,7 +71,7 @@ class TestTAM(unittest.TestCase):
         exp = algo.interpret(img_path, visual=False)
         result = np.array([exp.mean(), exp.std(), exp.min(), exp.max(), *exp.shape])
         desired = np.array([1.31726510e-02, 1.31807191e-02, 6.83252022e-04, 6.29992668e-02, 2.00000000e+00, 1.40000000e+01, 1.40000000e+01])
-        print("test_cv_multiple_inputs", result)
+
         assert_arrays_almost_equal(self, result, desired)
 
 
