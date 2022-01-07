4569a7e5d40341c75b3ab0601ea4980285bddfc0
holyseven <13829174+holyseven@users.noreply.github.com>
2022-01-07 12:52:53 +0800

    for ci

---
+++ scripts/install_from_scratch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# upgrade pip
+pip install --upgrade pip
+
+# install paddlepaddle cpu version
+pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
+
+# install interpretdl dev. 
+pip install -r requirements  # -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -e .[dev]
