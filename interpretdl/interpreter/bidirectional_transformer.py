853af5595b92f4c34ce0484dfd24e6152534281f
jiaminchen-1031 <75778793+jiaminchen-1031@users.noreply.github.com>
2022-08-17 15:22:19 +0800

    error fixed

--- interpretdl/interpreter/bidirectional_transformer.py
+++ interpretdl/interpreter/bidirectional_transformer.py
@@ -76,6 +76,9 @@ class BTCVInterpreter(TransformerInterpreter):
         attns, grads, inputs, values, projs, preds = self.predict_fn(data)
         assert start_layer < len(attns), "start_layer should be in the range of [0, num_block-1]"
 
+        if label is None:
+            label = preds
+
         b, h, s, _ = attns[0].shape
         R = np.eye(s, s, dtype=attns[0].dtype)
         R = np.expand_dims(R, 0)
@@ -197,6 +200,9 @@ class BTNLPInterpreter(TransformerInterpreter):
         
         attns, grads, inputs, values, projs, preds = self.predict_fn(data)
         assert start_layer < len(attns), "start_layer should be in the range of [0, num_block-1]"
+
+        if label is None:
+            label = preds
 
         b, h, s, _ = attns[0].shape
         R = np.eye(s, s, dtype=attns[0].dtype)
