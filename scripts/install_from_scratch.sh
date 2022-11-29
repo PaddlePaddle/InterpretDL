9497503286da98171e26cebebaeb467da848ab27
holyseven <13829174+holyseven@users.noreply.github.com>
2022-11-29 16:04:27 +0800

    back paddle to 2.3.2 ;)

--- scripts/install_from_scratch.sh
+++ scripts/install_from_scratch.sh
@@ -4,7 +4,10 @@
 pip install --upgrade pip
 
 # install paddlepaddle cpu version
-pip install paddlepaddle "protobuf<=3.20"  # -i https://mirror.baidu.com/pypi/simple
+pip install paddlepaddle==2.3.2 -i https://mirror.baidu.com/pypi/simple
+
+# install paddlepaddle cpu version from conda
+# conda install paddlepaddle==2.4.0 --channel https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/Paddle/
 
 # install interpretdl dev. 
 pip install -r requirements.txt  # -i https://pypi.tuna.tsinghua.edu.cn/simple
